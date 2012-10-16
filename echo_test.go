ac73fe2013d212e53c687ed21eac218465af5051
Eric Fode <ericfode@gmail.com>
2012-10-15 17:01:43 -0700

    trying to do a net thing

--- echo_test.go
+++ echo_test.go
@@ -2,11 +2,13 @@ package femebe
 
 import (
 	"bytes"
+	"net"
 	"testing"
 )
 
-//Benchmark for testing how long the send operation on a MessageStream takes
-func BenchmarkEchoSend(b *testing.B) {
+//Benchmark for testing how long the send operation on a MessageStream takes when used soly in
+//memory.
+func BenchmarkEchoSendMem(b *testing.B) {
 	b.StopTimer()
 	var ping Message
 	var pong Message
@@ -29,8 +31,9 @@ func BenchmarkEchoSend(b *testing.B) {
 	}
 }
 
-//Benchmark for testing how long the send operation on a MessageStream takes
-func BenchmarkEchoNext(b *testing.B) {
+//Benchmark for testing how long the send operation on a MessageStream takes when used soly in
+//memory.
+func BenchmarkEchoNextNet(b *testing.B) {
 	b.StopTimer()
 	var ping Message
 	var pong Message
@@ -51,7 +54,66 @@ func BenchmarkEchoNext(b *testing.B) {
 		}
 		b.StopTimer()
 	}
+}
 
+func initNet() (client *net.TCPConn, server *net.TCPListener) {
+	server, serr := autoDial(":2222")
+	if serr != nil {
+		panic(serr.Error())
+	}
+	client, cerr := autoDial("localhost:2222")
+	if cerr != nil {
+		panic(serr.Error())
+	}
+}
+
+//Test function to make sure that everything is working before benching
+func TestEchoNet(t *testing.T) {
+	var ping Message
+	var pong Message
+
+	serve, err = net.Listen("tcp", ":2222")
+	if err != nil {
+		t.Fail(err)
+	}
+
+	ping.InitFromBytes('i', []byte("ftest"))
+
+	buf := newClosableBuffer(bytes.NewBuffer(make([]byte, 0, 1024)))
+
+	ms := NewServerMessageStream("echo", buf)
+	ms.Send(&m)
+	t.Logf("%v", buf)
+
+	ms.Next(&pong)
+
+	rest, _ := pong.Force()
+	t.Logf("Type:%c, bytes:%s,", pong.MsgType(), rest)
+}
+
+//Benchmark for testing how long the send operation on a MessageStream takes when used soly in
+//memory.
+func BenchmarkEchoNextMem(b *testing.B) {
+	b.StopTimer()
+	var ping Message
+	var pong Message
+
+	ping.InitFromBytes('i', []byte("ftest"))
+	underBuf := bytes.NewBuffer(make([]byte, 0, 1024))
+	buf := newClosableBuffer(underBuf)
+	ms := NewServerMessageStream("echo", buf)
+
+	for i := 0; i < b.N; i++ {
+		underBuf.Reset()
+		for j := 0; j < 1000; j++ {
+			ms.Send(&m)
+		}
+		b.StartTimer()
+		for j := 0; j < 1000; j++ {
+			ms.Next(&pong)
+		}
+		b.StopTimer()
+	}
 }
 
 //Test function to make sure that everything is working before benching
--- util_test.go
+++ util_test.go
@@ -17,3 +17,23 @@ func (c *closableBuffer) Close() error {
 func newClosableBuffer(buf *bytes.Buffer) *closableBuffer {
 	return &closableBuffer{buf}
 }
+
+// Automatically chooses between unix sockets and tcp sockets for
+// listening
+func autoListen(place string) (net.Listener, error) {
+	if strings.Contains(place, "/") {
+		return net.Listen("unix", place)
+	}
+
+	return net.Listen("tcp", place)
+}
+
+// Automatically chooses between unix sockets and tcp sockets for
+// dialing.
+func autoDial(place string) (net.Conn, error) {
+	if strings.Contains(place, "/") {
+		return net.Dial("unix", place)
+	}
+
+	return net.Dial("tcp", place)
+}
